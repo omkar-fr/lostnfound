7dcb4af742626c4fc503e95bafd04969e451d9d1
omkar-fr <omkardahiphale135@gmail.com>
2026-02-11 01:57:15 +0530

    updated connection.py

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -8,7 +8,13 @@ DATABASE_URL = settings.DATABASE_URL
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=300)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=300,
+                       
+        connect_args={
+            "sslmode":"require",
+            "options":"-c statement_timeout=30000"
+        }
+                       )
 
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
